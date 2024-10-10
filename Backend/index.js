2e4a92bfe0040ac350a764472e4013536ac0402a
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2024-10-10 22:47:00 +0530

    Commit change

--- Backend/index.js
+++ Backend/index.js
@@ -2,20 +2,20 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import encodeurl from 'encodeurl'
+import encodeurl from 'encodeurl';
+import cookieParser from 'cookie-parser';
+
 
 
 dotenv.config();
 const app = express();
 
 
-//const port = proces.env.PORT || 8001;
+const port = process.env.PORT || 8001;
 const password = process.env.PASSWORD;
-console.log('The password is', password);
-
 const encodedPassword = encodeURIComponent(password);
 
-console.log('The encoded password is ', encodedPassword);
+
 
 const URI = `mongodb+srv://adarshsingh2003v:${encodedPassword}@food.mqsmc.mongodb.net/?retryWrites=true&w=majority&appName=food`;
 
@@ -28,3 +28,34 @@ const URI = `mongodb+srv://adarshsingh2003v:${encodedPassword}@food.mqsmc.mongod
 mongoose.connect(URI)
     .then(() => console.log("Mongodb Connected"))
     .catch((err) => console.log("Mongodb error : ", err));
+
+
+
+
+
+// middlewares 
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+
+
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || "Something went wrong ";
+
+
+    return res.status(status).json({
+        success: false,
+        status,
+        message,
+    });
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
