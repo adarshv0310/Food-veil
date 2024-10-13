67ed1ac0ed68be57f80b09cee6de43df25814489
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2024-10-13 11:03:45 +0530

    Commit

---
+++ Frontend/client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Components/Header';
+import Banner from '../Components/Banner';
+function Home() {
+  return (
+    <div>
+      <Header/>
+      <Banner/>
+    </div>
+  )
+}
+
+export default Home
