052e3038b6ff75b575322be4dab412eb0f8ef85a
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2024-10-14 12:48:37 +0530

    Commit

--- Frontend/client/src/pages/signin.jsx
+++ Frontend/client/src/pages/signin.jsx
@@ -1,25 +1,89 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-function signin() {
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+export default function Signin() {
+    const [loading , setLoading]=useState(false);
+    const [error ,setError] = useState(null);
+    const [successmessage ,setSuccessmessage]=useState(null);
+    const [formdata , setFormdata]= useState({});
+    const navigate=useNavigate();
+
+    const handlechange = (e)=>{
+        setFormdata({
+            ...formdata,
+            [e.target.id]: e.target.value,
+          });
+    }
+
+    const handlesubmit = async (e)=>{
+        e.preventDefault();
+        try{
+        setLoading(true);
+
+        const res =  await fetch('http://localhost:8000/auth/signin' , {
+            method:'POST',
+            headers:{
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formdata),
+        });
+
+        const data = await res.json();
+        //console.log(data);
+        if(data.success === false){
+            setLoading(false);
+            setError(data.message);
+            return ;
+        }
+        setLoading(false);
+        setError(null);
+        setSuccessmessage('Signin successful! Redirecting to home...');
+
+        setTimeout(() => {
+            navigate('/');
+          }, 2000);
+
+        }
+        catch(error){
+            setLoading(false);
+            setError(error.message);
+        }
+    }
+
+    // To mange effect of error
+   useEffect(()=>{
+      if(error){
+        const timer=setTimeout(()=>{
+            setError(null);
+        } , 3000);
+
+        return ()=> clearTimeout(timer);
+      }
+   },[error])
   return (
     <div>
         <div className='p-3 max-w-lg mx-auto mt-20'>
         <h1 className='text-3xl text-center font-semibold my-7'>Sign in</h1>
-         <form className='flex flex-col gap-4'>
+         <form className='flex flex-col gap-4'
+         onSubmit={handlesubmit}
+         >
             <input
             type='email'
             placeholder='abc@gmail.com'
             id='email'
             className='border p-3 rounded-lg border-black'
+            onChange={handlechange}
             />
         <input
           type='password'
           placeholder='password'
           className='border p-3 rounded-lg border-black'
           id='password'
+          onChange={handlechange}
          
         />
-        <button className='bg-orange-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign in</button>
+        <button className='bg-orange-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+        {loading ? 'Loading...' : 'Sign In'}
+        </button>
          </form>
          <div className='flex gap-2 mt-5'>
         <p> Dont have an account?</p>
@@ -27,9 +91,11 @@ function signin() {
           <span className='text-blue-700'>Sign up</span>
         </Link>
       </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
+      {successmessage && <p className='text-green-500 mt-5'>{successmessage}</p>}
     </div>
     </div>
   )
 }
 
-export default signin
+
