63717303183d791b50746d4102a5eb742ddfec39
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2024-10-14 22:25:30 +0530

    commit

--- Frontend/client/src/redux/store.js
+++ Frontend/client/src/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import authReducer from './User/authSlice.js';
+import userReducer from './User/userSlice.js';
 
 
 
 export const store = configureStore({
-    reducer: {},
+    reducer: {
+        auth: authReducer,
+        user: userReducer,
+    },
 
 });
