9c2e5a15a290660408157cdadc25276f9be78282
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2024-10-16 00:21:30 +0530

    Commit

--- Frontend/client/src/Components/PrivateRoute.jsx
+++ Frontend/client/src/Components/PrivateRoute.jsx
@@ -1,33 +1,4 @@
 
-/*import { useSelector } from 'react-redux';
-import {  Navigate } from 'react-router-dom';
-
-function PrivateRoute() {
-   const {currentUser} = useSelector((state)=>state.auth)
-   if (!currentUser) {
-    return <Navigate to='/signin' />;
-}
-
-
-switch (currentUser.role) {
-    case 'admin':
-        return <Navigate to='/adminp'/>;
-    case 'customer':
-        return <Navigate to='/customerp'/>;
-    case 'resturant_owner':
-        return <Navigate to='/resturantp' />;
-    
-     case 'delivery_person':
-         return <Navigate to='/deliveryp' />;
-    // Add more roles as needed
-    default:
-        return <Navigate to='/' />;
-}
-
-}
-
-export default PrivateRoute;*/
-
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import Adminprofile from '../pages/Profile/Adminprofile';
