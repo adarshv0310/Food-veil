95f2eb27c59415dd45d238de7c258024c04a9e04
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2024-10-16 01:15:40 +0530

    commit

--- Frontend/client/src/pages/Profile/Customerprofile.jsx
+++ Frontend/client/src/pages/Profile/Customerprofile.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useRef, useState }from 'react'
 import ProfileHeader from '../../Components/ProfileHeader'
+import { useSelector } from 'react-redux';
 function Customerprofile() {
+  const {currentUser , loading , error} =useSelector((state)=>state.auth);
+  const fileRef =useRef();
   return (
     <div className='container flex flex-col'>
       <ProfileHeader/>
+      <div className=' flex flex-row'>
       <div className='   flex flex-col container shadow-md bg-white w-1/4 h-screen mt-10 '>
      
      <button className='bg-slate-200  m-9 p-4 border rounded-xl font-bold text-lg'>Dashboard</button>
@@ -12,6 +16,60 @@ function Customerprofile() {
      <button className='bg-slate-200  m-9  mt-1 p-4 border rounded-xl font-bold text-lg'>Delete Account</button>
     
     </div>
+
+    <div className='container mx-auto max-w-lg'>
+    <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+    <form
+      className='flex flex-col gap-4'
+      >
+       <input 
+        type='file'
+        ref={fileRef}
+        accept='image/*'
+        hidden
+        />
+        <img
+        src={currentUser.avatar}
+        onClick={()=>fileRef.current.click()}
+        alt='profile'
+        
+        className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2'
+        />
+          <input
+          type='text'
+       
+          placeholder='name'
+          defaultValue={currentUser.name}
+          id='username'
+          className='border p-3 rounded-lg'
+          
+        />
+        <input
+          type='email'
+          placeholder='email'
+          id='email'
+          defaultValue={currentUser.email}
+          className='border p-3 rounded-lg'
+         
+          
+        />
+        <input
+          type='password'
+          placeholder='password'
+          
+          id='password'
+          className='border p-3 rounded-lg'
+          
+        />
+
+<button
+className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'
+>
+  Update
+</button>
+</form>
+    </div>
+      </div>
     </div>
   )
 }
