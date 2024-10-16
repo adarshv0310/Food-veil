f02493ea7ea8feb4b5f520851546bdbf7d039fb0
adarshv0310 <141803517+adarshv0310@users.noreply.github.com>
2024-10-16 18:43:07 +0530

    Commit

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -5,6 +5,7 @@ import bcrypt from 'bcrypt'
 
 export const updateUser = async(req, res, next) => {
     if (req.user.id !== req.params.id) {
+
         return next(errorhandler(401, 'Your account not found'));
     }
 
@@ -13,6 +14,7 @@ export const updateUser = async(req, res, next) => {
         if (req.body.password) {
             req.body.password = await bcrypt.hash(req.body.password, 10);
         }
+
 
         // Update the user in the database
         const updatedUser = await User.findByIdAndUpdate(
